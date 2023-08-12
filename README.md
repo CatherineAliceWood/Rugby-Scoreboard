c6353dd6e6121eb826475b3cf782abc184799d12
Catherine Wood <catherinealicewood@outlook.com>
2023-08-12 20:11:12 +0100

    added README

---
+++ README.md
@@ -0,0 +1,13 @@
+# Rugby Scoreboard
+[https://catherinealicewood.github.io/Rugby-Scoreboard/]
+## Created as part of the Scrimba Frontend Web Dev Career Path
+I took up the basketball scoreboard solo project task from the course but decided to do a rugby scoreboard instead!
+The buttons for each team add to their score for a try, conversion or penalty.
+The scores then change colour depending on who's winning or whether it's a draw.
+
+### Notes:
+- used Bootstrap for the layout to create the two columns
+- just did some simply styling in CSS - JS functionality is the main feature
+- used vanilla JS to get that solid before making something with jQuery
+- stored elements in variables to try and keep code DRY but bet it could be DRYer!
+- I did very little Googling! - Trying to get familiar with the syntax and solve problems by trial and error to increase my learning
--- scripts.js
+++ scripts.js
@@ -3,57 +3,57 @@
 3. make whoever's score is higher green and the other score red
 4. Add new game button to reset scores
 */
-let scoreP1Text = document.querySelector("#scoreP1");
-let scoreP2Text = document.querySelector("#scoreP2");
-let scoreP1 = 0;
-let scoreP2 = 0;
-document.querySelector("#plus2P1").addEventListener("click", function () {
-    scoreP1Text.innerText = scoreP1 += 2;
+let scoreTeam1Text = document.querySelector("#scoreTeam1");
+let scoreTeam2Text = document.querySelector("#scoreTeam2");
+let scoreTeam1 = 0;
+let scoreTeam2 = 0;
+document.querySelector("#plus2Team1").addEventListener("click", function () {
+    scoreTeam1Text.innerText = scoreTeam1 += 2;
     whosWinning();
 })
-document.querySelector("#plus3P1").addEventListener("click", function () {
-    scoreP1Text.innerText = scoreP1 += 3;
+document.querySelector("#plus3Team1").addEventListener("click", function () {
+    scoreTeam1Text.innerText = scoreTeam1 += 3;
     whosWinning();
 })
-document.querySelector("#plus5P1").addEventListener("click", function () {
-    scoreP1Text.innerText = scoreP1 += 5;
+document.querySelector("#plus5Team1").addEventListener("click", function () {
+    scoreTeam1Text.innerText = scoreTeam1 += 5;
     whosWinning();
 })
-document.querySelector("#plus2P2").addEventListener("click", function () {
-    scoreP2Text.innerText = scoreP2 += 2;
+document.querySelector("#plus2Team2").addEventListener("click", function () {
+    scoreTeam2Text.innerText = scoreTeam2 += 2;
     whosWinning();
 })
-document.querySelector("#plus3P2").addEventListener("click", function () {
-    scoreP2Text.innerText = scoreP2 += 3;
+document.querySelector("#plus3Team2").addEventListener("click", function () {
+    scoreTeam2Text.innerText = scoreTeam2 += 3;
     whosWinning();
 })
-document.querySelector("#plus5P2").addEventListener("click", function () {
-    scoreP2Text.innerText = scoreP2 += 5;
+document.querySelector("#plus5Team2").addEventListener("click", function () {
+    scoreTeam2Text.innerText = scoreTeam2 += 5;
     whosWinning();
 })
 function whosWinning() {
-    if (scoreP1 > scoreP2) {
-        scoreP1Text.classList.add("winning-score");
-        scoreP1Text.classList.remove("losing-score","draw");
-        scoreP2Text.classList.add("losing-score");
-        scoreP2Text.classList.remove("winning-score","draw");
-    } else if (scoreP1 < scoreP2) {
-        scoreP1Text.classList.add("losing-score");
-        scoreP1Text.classList.remove("winning-score","draw");
-        scoreP2Text.classList.add("winning-score");
-        scoreP2Text.classList.remove("losing-score","draw");
-    } else if (scoreP1 === scoreP2) {
-        scoreP1Text.classList.remove("losing-score","winning-score");
-        scoreP1Text.classList.add("draw");
-        scoreP2Text.classList.remove("losing-score","winning-score");
-        scoreP2Text.classList.add("draw");
+    if (scoreTeam1 > scoreTeam2) {
+        scoreTeam1Text.classList.add("winning-score");
+        scoreTeam1Text.classList.remove("losing-score","draw");
+        scoreTeam2Text.classList.add("losing-score");
+        scoreTeam2Text.classList.remove("winning-score","draw");
+    } else if (scoreTeam1 < scoreTeam2) {
+        scoreTeam1Text.classList.add("losing-score");
+        scoreTeam1Text.classList.remove("winning-score","draw");
+        scoreTeam2Text.classList.add("winning-score");
+        scoreTeam2Text.classList.remove("losing-score","draw");
+    } else if (scoreTeam1 === scoreTeam2) {
+        scoreTeam1Text.classList.remove("losing-score","winning-score");
+        scoreTeam1Text.classList.add("draw");
+        scoreTeam2Text.classList.remove("losing-score","winning-score");
+        scoreTeam2Text.classList.add("draw");
     }
 }
 document.querySelector("#reset").addEventListener("click", function() {
-    scoreP1Text.innerText = 0;
-    scoreP2Text.innerText = 0;
-    scoreP1Text.classList.remove("winning-score","losing-score","draw");
-    scoreP2Text.classList.remove("winning-score","losing-score","draw");
-    scoreP1 = 0;
-    scoreP2 = 0;
+    scoreTeam1Text.innerText = 0;
+    scoreTeam2Text.innerText = 0;
+    scoreTeam1Text.classList.remove("winning-score","losing-score","draw");
+    scoreTeam2Text.classList.remove("winning-score","losing-score","draw");
+    scoreTeam1 = 0;
+    scoreTeam2 = 0;
 })
